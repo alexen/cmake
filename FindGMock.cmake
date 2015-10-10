3008c71ee208c047ef688ec071e87672604b938b
Enakaev Aleksey <Aleksey.Enakaev@infotecs.ru>
2015-10-10 21:34:32 +0300

    Добавлены самописные модули для поиска библиотек gtest и gmock

--- FindGMock.cmake
+++ FindGMock.cmake
@@ -1,86 +1,49 @@
-# Locate the Google C++ Mocking Framework.
-#
-# Defines the following variables:
-#
-#   GMOCK_FOUND - Found the Google Mocking framework
-#   GMOCK_INCLUDE_DIRS - Include directories
-#
-# Also defines the library variables below as normal
-# variables.  These contain debug/optimized keywords when
-# a debugging library is found.
-#
-#   GMOCK_BOTH_LIBRARIES - Both libgmock & libgmock-main
-#   GMOCK_LIBRARIES - libgmock
-#   GMOCK_MAIN_LIBRARIES - libgmock-main
-#
-# Accepts the following variables as input:
-#
-#   GMOCK_ROOT - (as CMake or env. variable)
-#                The root directory of the gmock install prefix
-#
-#-----------------------
-# Example Usage:
-#
-#    enable_testing(true)
-#    find_package(GMock REQUIRED)
-#    include_directories(${GMOCK_INCLUDE_DIRS})
-#
-#    add_executable(foo foo.cc)
-#    target_link_libraries(foo ${GMOCK_BOTH_LIBRARIES})
-#
-#    add_test(AllTestsInFoo foo)
-#
+set(GTEST_ROOT ${GMOCK_ROOT}/gtest)
 
-set (GMOCK_FOUND FALSE)
+if(NOT GMOCK_BUILD)
+    set(GMOCK_BUILD ${GMOCK_ROOT})
+endif()
 
-set (GMOCK_ROOT $ENV{GMOCK_ROOT} CACHE PATH "Path to the gmock root directory.")
-if (NOT EXISTS "${GMOCK_ROOT}")
-    message (FATAL_ERROR "GMOCK_ROOT does not exist.")
-endif ()
+set(GTEST_BUILD ${GMOCK_BUILD}/gtest)
 
-set (GMOCK_BUILD ${GMOCK_ROOT}/build CACHE PATH "Path to the gmock build directory.")
-if (NOT EXISTS "${GMOCK_BUILD}")
-    message (FATAL_ERROR "GMOCK_BUILD does not exist.")
-endif ()
+find_package(GTest)
 
-# Find the include directory
-find_path(GMOCK_INCLUDE_DIRS gmock/gmock.h
-        HINTS
-        ${GMOCK_ROOT}/include
+find_path(GMOCK_INCLUDE_DIR
+    gmock/gmock.h
+    HINTS ${GMOCK_ROOT}/include
 )
-mark_as_advanced(GMOCK_INCLUDE_DIRS)
 
-function(_gmock_find_library _name)
-    find_library(${_name}
-        NAMES ${ARGN}
-        HINTS
-            $ENV{GMOCK_ROOT}
-            ${GMOCK_ROOT}
-    )
-    mark_as_advanced(${_name})
-endfunction()
+if(NOT GMOCK_INCLUDE_DIR)
+    message(FATAL_ERROR "No gmock include found. Try to specify GMOCK_ROOT variable.")
+endif()
 
-# Find the gmock libraries
-if (MSVC)
-    find_library (GMOCK_LIBRARIES_DEBUG   gmock ${GMOCK_BUILD}/Debug)
-    find_library (GMOCK_LIBRARIES_RELEASE gmock ${GMOCK_BUILD}/Release)
-    find_library (GMOCK_MAIN_LIBRARIES_DEBUG   gmock_main ${GMOCK_BUILD}/Debug)
-    find_library (GMOCK_MAIN_LIBRARIES_RELEASE gmock_main ${GMOCK_BUILD}/Release)
-    set (GMOCK_LIBRARIES
-        debug ${GMOCK_LIBRARIES_DEBUG}
-        optimized ${GMOCK_LIBRARIES_RELEASE}
-    )
-    set (GMOCK_MAIN_LIBRARIES
-        debug ${GMOCK_MAIN_LIBRARIES_DEBUG}
-        optimized ${GMOCK_MAIN_LIBRARIES_RELEASE}
-    )
-    set (GMOCK_BOTH_LIBRARIES
-        ${GMOCK_LIBRARIES}
-        ${GMOCK_MAIN_LIBRARIES}
-    )
-#    message (STATUS "GMOCK_BOTH_LIBRARIES ARE ${GMOCK_BOTH_LIBRARIES}")
-else ()
-    find_library (GMOCK_LIBRARIES      gmock      ${GMOCK_BUILD})
-    find_library (GMOCK_MAIN_LIBRARIES gmock_main ${GMOCK_BUILD})
-    set(GMOCK_BOTH_LIBRARIES ${GMOCK_LIBRARIES} ${GMOCK_MAIN_LIBRARIES})
-endif ()
+find_library(GMOCK_LIBRARY
+    gmock
+    HINTS ${GMOCK_BUILD} ${GMOCK_BUILD}/lib
+)
+
+if(NOT GMOCK_LIBRARY)
+    message(FATAL_ERROR "No gmock library found. Try to specify GMOCK_BUILD variable.")
+endif()
+
+find_library(GMOCK_MAIN_LIBRARY
+    gmock_main
+    HINTS ${GMOCK_BUILD} ${GMOCK_BUILD}/lib
+)
+
+if(NOT GMOCK_MAIN_LIBRARY)
+    message(FATAL_ERROR "No gmock_main library found. Try to specify GMOCK_BUILD variable.")
+endif()
+
+set(GMOCK_INCLUDE_DIRS ${GMOCK_INCLUDE_DIR})
+set(GMOCK_LIBRARIES ${GMOCK_LIBRARY} ${GMOCK_MAIN_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(GMock
+    DEFAULT_MSG
+    GMOCK_INCLUDE_DIRS
+    GMOCK_LIBRARIES
+)
+
+mark_as_advanced(GMOCK_INCLUDE_DIR GMOCK_LIBRARY GMOCK_MAIN_LIBRARY)
---
+++ FindGTest.cmake
@@ -0,0 +1,43 @@
+find_path(GTEST_INCLUDE_DIR
+    gtest/gtest.h
+    HINTS ${GTEST_ROOT}/include
+)
+
+if(NOT GTEST_INCLUDE_DIR)
+    message(FATAL_ERROR "No gtest include found. Try to specify GTEST_ROOT variable.")
+endif()
+
+if(NOT GTEST_BUILD)
+    set(GTEST_BUILD ${GTEST_ROOT})
+endif()
+
+find_library(GTEST_LIBRARY
+    gtest
+    HINTS ${GTEST_BUILD} ${GTEST_BUILD}/lib
+)
+
+if(NOT GTEST_LIBRARY)
+    message(FATAL_ERROR "No gtest library found. Try to specify GTEST_BUILD variable.")
+endif()
+
+find_library(GTEST_MAIN_LIBRARY
+    gtest_main
+    HINTS ${GTEST_BUILD} ${GTEST_BUILD}/lib
+)
+
+if(NOT GTEST_MAIN_LIBRARY)
+    message(FATAL_ERROR "No gtest_main library found. Try to specify GTEST_BUILD variable.")
+endif()
+
+set(GTEST_INCLUDE_DIRS ${GTEST_INCLUDE_DIR})
+set(GTEST_LIBRARIES ${GTEST_LIBRARY} ${GTEST_MAIN_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(GMock
+    DEFAULT_MSG
+    GTEST_INCLUDE_DIRS
+    GTEST_LIBRARIES
+)
+
+mark_as_advanced(GTEST_INCLUDE_DIR GTEST_LIBRARY GTEST_MAIN_LIBRARY)
